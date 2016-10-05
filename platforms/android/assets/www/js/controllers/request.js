e07dd990d10812d98aa228602677968b44acb1cb
Andrew castro <listerine1989@gmail.com>
2016-10-05 16:05:03 -0500

    latest

--- platforms/android/assets/www/js/controllers/request.js
+++
@@ -1,7 +0,0 @@
-angular.module('starter.controllers').controller('requestCtrl', function(api, $scope, constants, account, storage, $ionicPopup, $state){
-	$scope.load = function(){
-		api.pedido().get().success(function(res){
-			$scope.records = res || [];
-		})
-	}
-})
